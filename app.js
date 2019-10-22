bc68750f321ffab7256b714f789b8556c0fbc396
alekya pochampally <S536776@nwmissouri.edu>
2019-10-22 15:37:18 -0500

    commit

--- app.js
+++ app.js
@@ -18,15 +18,11 @@ const port = process.env.PORT || config.get("port");
 
 // declare your callback function the old way
 app.get('/', function (req, res) {
-  res.send('Welcome to the default page!  <br> <br>' +
+  res.send('Welcome to the my page!  <br> <br>' +
     'Try going to different URIs by adding these at the end: <br> <br>' +
+    '/hi <br>' +
     '/hello <br>' +
-    '/big <br>' +
-    '/json <br>' +
-    '/greeting/yourname <br>' +
-    '/yo/Dr.Rogers <br>' +
-    '/fortune <br>' +
-    '/fancy/?first=Denise&last=Case <br>' +
+    '/greetings <br>' +
     '<br> <br>' +
     'Fork the source code from <a href="https://github.com/denisecase/node-express-app">https://github.com/denisecase/node-express-app</a>'
   )
@@ -34,53 +30,20 @@ app.get('/', function (req, res) {
 
 // or use the new arrow function syntax
 // respond with text
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+app.get('/hi', (req, res) => {
+  res.send('Hi Welcom here!')
 })
 
 // or respond with html
-app.get('/big', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello there!</h1>')
 })
 
 // or respond with JSON
-app.get('/json', (req, res) => {
-  res.send('{"name" : "Nandini"}')
+app.get('/greetings', (req, res) => {
+  res.send('<h1>Greetings!</h1>')
 })
 
-// :name indicates a parameter at this location in the URI
-app.get('/greeting/:id', (req, res) => {
-  res.send(`Hello! The id provided was ${req.params.id}.`)
-})
-
-// combine your skills and get creative
-app.get('/yo/:buddy', (req, res) => {
-  res.send(`<h1>Yo, ${req.params.buddy}!</h1>`)
-})
-
-// provide multiple query parameters (named first and last) with ? and &
-app.get('/fancy', (req, res) => {
-  const first = req.query.first
-  const last = req.query.last
-  res.send(`Hello ${first} ${last}!`)
-})
-
-let fortunes = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.',
-'You may rely on it', 'As I see it, yes.', 'Most likely', 'Outlook good.', 'Yes.', 'Signs point to yes.',
-'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 
-'Concentrate and ask again.', 'Don\'t count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.',
-'Very doubtful.']
-
-// Implements a Magic 8 Ball service
-app.get('/fortune', (req,res) => {
-  if(isEmpty(req.query)){
-    res.send('<h2>You wish to know the future?</h2>' +
-             '<p>Ask a question in the query string, e.g., http://localhost:3002/fortune?Will I become rich? <br/>' +
-             '<p>The Magic 8 Ball will answer!</p>')
-  } else {
-    res.send(`The answer is ... wait for it ... ${fortunes[randomInt(0, fortunes.length)]}`)
-  }
-})
 
 // Use middleware to handle all non-managed routes (e.g. /xyz)
 // https://expressjs.com/en/api.html#req.originalUrl
@@ -93,12 +56,8 @@ app.listen(port, hostname, () => {
   console.log(`\n App listening at http://${hostname}:${port}/`)
   console.log(`\n Try going to different URIs:\n`)
   console.log(`   Try /hello`)
-  console.log(`   Try /big`)
-  console.log(`   Try /json`)
-  console.log(`   Try /fortune`)
-  console.log(`   Try /greeting/yourname`)
-  console.log(`   Try /yo/Dr.Rogers`)
-  console.log(`   Try /fancy/?first=Denise&last=Case`)
+  console.log(`   Try /hi`)
+  console.log(`   Try /greetings`)
   console.log('\n Hit CTRL-C CTRL-C to stop\n')
 })
 
@@ -112,7 +71,3 @@ function isEmpty(obj) {
   return true;
 }
 
-// generates a random value in [low,high) 
-function randomInt(low, high) {
-  return Math.floor(Math.random() * (high - low) + low)
-}
